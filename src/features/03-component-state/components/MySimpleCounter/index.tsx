09e3e03ce906e7ebea2cd3a36ca1bafc035f6c56
pwoloszun <piotr.woloszun@gmail.com>
2021-10-25 13:07:54 +0200

    state p1

--- src/features/03-component-state/components/MySimpleCounter/index.tsx
+++ src/features/03-component-state/components/MySimpleCounter/index.tsx
@@ -1,19 +1,48 @@
-/*eslint @typescript-eslint/no-unused-vars: 'off'*/
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './styles.module.css';
 
+// const myArr = [1, 2, 3, 4];
+
+// const el0 = myArr[0];
+// const el1 = myArr[1];
+
+// const [el0, el1, , el3] = myArr;
+
+
+// const person = { name: 'bob', age: 123 };
+// const age = person.age;
+// const name = person.name;
+
+// const { age, name } = person;
+
+
+// pseudo code - what happens under the hood
+const _state = {
+  0: 123, // piece of state
+  1: 'bob'
+};
+
 export default function MySimpleCounter(): React.ReactElement {
-  const value = 100;
+  console.log('RENDERED ');
+
+  const [value, setValue] = useState(200); // local component state
+  const [name, setName] = useState('bob'); // local component state
+
   const incrementHandler = () => {
     // TODO
+    console.log('1st inc click!!!:',);
+    setValue(value + 10);
+    console.log('2nd inc click!!!:',);
+    setValue(value + 10);
   };
 
   return (
     <div className={styles.mySimpleCounter}>
-      TODO_PLACEHOLDER
-      <button>incr</button>
+      <h1>Value: {value}</h1>
+      <h3>Name: {name}</h3>
+      <button onClick={incrementHandler}>incr</button>
     </div>
   );
 }
