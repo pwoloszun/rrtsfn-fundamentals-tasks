e692a5c20edf8e372b78932de79660cabf726e10
pwoloszun <piotr.woloszun@gmail.com>
2021-10-25 13:24:33 +0200

    state p3

--- src/features/03-component-state/components/MySimpleCounter/index.tsx
+++ src/features/03-component-state/components/MySimpleCounter/index.tsx
@@ -1,7 +1,7 @@
-
 import React, { useState } from 'react';
 
 import styles from './styles.module.css';
+import { produce } from 'immer';
 
 // const myArr = [1, 2, 3, 4];
 
@@ -20,28 +20,46 @@ import styles from './styles.module.css';
 
 // pseudo code - what happens under the hood
 const _state = {
-  0: 123, // piece of state
-  1: 'bob'
+  0: 210, // piece of state
+  1: { name: 'bob', age: 123 }
 };
 
 export default function MySimpleCounter(): React.ReactElement {
   console.log('RENDERED ');
 
   const [value, setValue] = useState(200); // local component state
-  const [name, setName] = useState('bob'); // local component state
+  const [person, setPerson] = useState({
+    name: 'bob',
+    age: 123,
+    myChildren: [
+      {
+        name: 'ed',
+        friends: [{ name: 'kate' }]
+      }
+    ]
+  }); // local component state
 
   const incrementHandler = () => {
-    // TODO
-    console.log('1st inc click!!!:',);
-    setValue(value + 10);
-    console.log('2nd inc click!!!:',);
-    setValue(value + 10);
+    setValue((currValue) => {
+      const nextValue = currValue + 10;
+      return nextValue
+    });
+    setValue((currValue) => currValue + 10);
+
+    setPerson((currPerson) => {
+      // const nextPerson = { ...person, name: `Batman ${Math.random()}` };
+      const nextPerson = produce(currPerson, (draft) => {
+        draft.name = `Batman ${Math.random()}`;
+      });
+      return nextPerson;
+    });
+
   };
 
   return (
     <div className={styles.mySimpleCounter}>
       <h1>Value: {value}</h1>
-      <h3>Name: {name}</h3>
+      <h3>Name: {person.name}</h3>
       <button onClick={incrementHandler}>incr</button>
     </div>
   );
