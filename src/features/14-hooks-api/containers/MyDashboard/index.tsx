4505539a8ad750abf67c0a97dea1daf79c9cbe80
pwoloszun <piotr.woloszun@gmail.com>
2021-10-26 15:18:08 +0200

    useFetch p1

--- src/features/14-hooks-api/containers/MyDashboard/index.tsx
+++ src/features/14-hooks-api/containers/MyDashboard/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import fetchRealEstates, { RealEstate } from 'src/api/fetch-real-estates';
+import fetchUsers, { User } from 'src/api/fetch-users';
 
 import EntitiesSimpleList from '../../components/EntitiesSimpleList';
 
@@ -9,6 +10,18 @@ export default function MyDashboard() {
   const realEstates: RealEstate[] = [];
   const isLoading = true;;
   const error = null;
+
+
+  fetchRealEstates()
+    .then((realEstates) => {
+      console.log('re json:', realEstates);
+    })
+    .catch((err) => {
+
+    })
+    .finally(() => {
+
+    });
 
   // TODO 1: impl fetching real estates
 
