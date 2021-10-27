cb2ae67b06b04c55188be5965396211579075073
pwoloszun <piotr.woloszun@gmail.com>
2021-10-27 11:49:56 +0200

    minors

--- src/features/21-redux-fundamentals/containers/NewSyncCounter/index.tsx
+++ src/features/21-redux-fundamentals/containers/NewSyncCounter/index.tsx
@@ -3,17 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import Counter from 'src/components/Counter';
 import { actions, selectors } from 'src/store/21b-newSyncCounter'; // redux module
-import { RootState } from '../../../../store/store';
-
-// action creator
-// function incrementAction(payload: { incBy: number }) {
-//   return {
-//     type: 'newSyncCounter/increment',
-//     payload
-//   };
-// }
-
-// const selectCounterValue = (state: RootState) => state.newSyncCounter.value;
 
 export default function NewSyncCounter() {
   const dispatch = useDispatch();
@@ -23,7 +12,6 @@ export default function NewSyncCounter() {
   const updatedAt = 999999;
 
   const incrementHandler = () => {
-    // const action = incrementAction({ incBy: 10 });
     const action = actions.increment({ incBy: 20 });
     dispatch(action);
   };
