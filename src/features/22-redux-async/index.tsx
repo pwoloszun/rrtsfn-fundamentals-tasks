bf12dd14af7e9d790c1f49c42d7d115041186c5e
pwoloszun <piotr.woloszun@gmail.com>
2021-10-27 13:59:11 +0200

    async counter

--- src/features/22-redux-async/index.tsx
+++ src/features/22-redux-async/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import ClassicAsyncCounterUsingSaga from './containers/ClassicAsyncCounterUsingSaga';
+// import ClassicAsyncCounterUsingSaga from './containers/ClassicAsyncCounterUsingSaga';
+import AsyncCounterUsingSaga from './containers/AsyncCounterUsingSaga';
 
 class ReduxAsync extends React.Component {
   render() {
     return (
       <div>
         <h3>ReduxAsync</h3>
-        <ClassicAsyncCounterUsingSaga />
+        <AsyncCounterUsingSaga />
       </div>
     );
   }
