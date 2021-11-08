ccb1d499cde167aeaf803fd581b3c74be2f18d6b
pwoloszun <piotr.woloszun@gmail.com>
2021-11-08 13:13:10 +0100

    com state

--- src/features/03-component-state/components/MySimpleCounter/index.tsx
+++ src/features/03-component-state/components/MySimpleCounter/index.tsx
@@ -1,19 +1,53 @@
-/*eslint @typescript-eslint/no-unused-vars: 'off'*/
-
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './styles.module.css';
+import { cloneDeep } from 'lodash';
+import produce from 'immer';
+
+// React render mechanism:
+// 1) (re)render phase
+// 2) commit phase
+
+
+// pseudo code - _state created per comp. instance
+const _state0 = {
+  0: 110,
+  // 1: 'batman!',
+  2: { name: 'bob', age: 123 }
+};
 
 export default function MySimpleCounter(): React.ReactElement {
-  const value = 100;
+  console.log('RENDER:',);
+  // const value = 100; // local comp state 
+  // state === SINGLE SOURCE OF TRUTH
+  const [value, setValue] = useState(100); // state piece
+  // const [name, setName] = useState('batman!'); // state piece
+  const [person, setPerson] = useState({
+    name: { value: 'bob' },
+    age: { value: 123 },
+  }); // state piece
+
   const incrementHandler = () => {
-    // TODO
+    // value is STALE state
+    setValue((currValue) => {
+      const nextValue = currValue + 10;
+      return nextValue;
+    });
+    setValue((currValue) => currValue + 10);
+
+    setPerson((currPerson) => {
+      const nextPerson = produce(currPerson, (draft) => {
+        draft.name.value = `batman ${Math.random()}`;
+      });
+      return nextPerson;
+    });
   };
 
   return (
     <div className={styles.mySimpleCounter}>
-      TODO_PLACEHOLDER
-      <button>incr</button>
+      <h3>Value {value}</h3>
+      <h3>my Person name: {person.name.value}</h3>
+      <button onClick={incrementHandler}>incr</button>
     </div>
   );
 }
