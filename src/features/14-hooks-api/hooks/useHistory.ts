e97f3d9b2e01908f84233460586da15990977215
pwoloszun <piotr.woloszun@gmail.com>
2021-11-09 12:00:11 +0100

    use hist p2

--- src/features/14-hooks-api/hooks/useHistory.ts
+++ src/features/14-hooks-api/hooks/useHistory.ts
@@ -13,23 +13,62 @@ interface UseHistoryResult<T> {
 };
 
 export default function useHistory<T>(initialValue: T): UseHistoryResult<T> {
-  // TODO
+  const [curr, setCurr] = useState(initialValue);
+  const [past, setPast] = useState<T[]>([]);
+  const [future, setFuture] = useState<T[]>([]);
+
+  const canUndo = past.length > 0;
+  const canRedo = future.length > 0;
+
+  const setValue = (nextValue: T) => {
+    const nextPast = [...past, curr];
+    setPast(nextPast);
+    const nextCurr = nextValue;
+    setCurr(nextCurr);
+    const nextFuture: T[] = [];
+    setFuture(nextFuture);
+  };
+
+  const undo = () => {
+    if (!canUndo) {
+      return;
+    }
+
+    const nextPast = past.slice(0, -1);
+    setPast(nextPast);
+    const nextCurr = past[past.length - 1];
+    setCurr(nextCurr);
+    const nextFuture = [curr, ...future];
+    setFuture(nextFuture);
+  };
+
+  const redo = () => {
+    if (!canRedo) {
+      return;
+    }
+    const nextPast = [...past, curr];
+    setPast(nextPast);
+    const nextCurr = future[0];
+    setCurr(nextCurr);
+    const nextFuture = future.slice(1);
+    setFuture(nextFuture);
+  };
 
   return {
     // part 1
-    value: initialValue,
-    setValue: (nextValue: T) => { },
-    past: [],
+    value: curr,
+    setValue,
+    past,
 
     // part 2
-    undo: () => { },
+    undo,
 
     // part 3
-    future: [],
-    redo: () => { },
+    future,
+    redo,
 
     // part 4
-    canUndo: false,
-    canRedo: false,
+    canUndo,
+    canRedo,
   };
 }
