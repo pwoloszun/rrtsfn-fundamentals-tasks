e2c0cb2fa069558de7a938c26d7c7d4ba249b878
pwoloszun <piotr.woloszun@gmail.com>
2021-11-09 15:19:45 +0100

    class vs fn

--- src/features/14-hooks-api/containers/MyDashboard/index.tsx
+++ src/features/14-hooks-api/containers/MyDashboard/index.tsx
@@ -6,6 +6,91 @@ import fetchUsers, { User } from 'src/api/fetch-users';
 import EntitiesSimpleList from '../../components/EntitiesSimpleList';
 import useAsync from '../../hooks/useAsync';
 
+
+// interface IProps {
+//   id: number;
+//   fullName: string;
+// }
+
+// interface IState {
+//   value: number;
+// }
+
+// class MyCmp extends React.Component<IProps, IState> {
+
+//   state = {
+//     value: 100,
+//     age: 123,
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.btnClickHanlder}></button>
+//       </div>
+//     );
+//   }
+
+//   componentDidMount() {
+//     //...
+//   }
+
+//   componentDidUpdate() {
+//     //...
+//     if (id) {
+//       // ...
+//     }
+//     if (fullName) {
+//       // ...
+
+//     }
+//   }
+
+//   private btnClickHanlder = () => {
+//     this.setState({ value: 200 });
+//   };
+// }
+
+// function FnMyCmp(props: IProps) {
+//   const [state, setState] = useState({
+//     value: 100,
+//     age: 123
+//   });
+
+//   useEffect(() => {
+//     // ...
+//   }, []);
+
+//   useEffect(() => {
+//     // ...
+//   }, [props.id]);
+
+//   const btnClickHanlder = () => {
+//     setState({ value: 200 });
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={btnClickHanlder}></button>
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default function MyDashboard() {
   const [
     realEstates,
