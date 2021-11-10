b51891938d7673e0a894ae7a39238900f000d29a
pwoloszun <piotr.woloszun@gmail.com>
2021-11-10 15:38:35 +0100

    minors

--- src/features/21-redux-fundamentals/containers/NewSyncCounter/index.tsx
+++ src/features/21-redux-fundamentals/containers/NewSyncCounter/index.tsx
@@ -11,7 +11,7 @@ export default function NewSyncCounter() {
   );
   const updatedAt = useSelector(
     selectors.selectCounterUpdatedAt
-  );;
+  );
 
   const incrementHandler = () => {
     const action = actions.increment({
