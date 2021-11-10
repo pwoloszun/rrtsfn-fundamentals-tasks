3050365e4d0b20fd778623b7e6d761fe3e66604a
pwoloszun <piotr.woloszun@gmail.com>
2021-11-10 12:07:52 +0100

    smart counter spec

--- src/features/12-components-testing/components/__tests__/MyAjaxCounter.spec.tsx
+++ src/features/12-components-testing/components/__tests__/MyAjaxCounter.spec.tsx
@@ -1,94 +1,46 @@
 import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { CounterValueParams } from 'src/api/counter-api';
+import { CounterValueEntity, CounterValueParams } from 'src/api/counter-api';
 import { stubServerApi } from 'src/mocks/utils/server-stub';
 
 import MyAjaxCounter from '../MyAjaxCounter';
 
 describe('MyAjaxCounter.spec', () => {
 
-  describe('defined initialValue', () => {
-
-    it('should render initial value fetched from server', async () => {
-      const id = 100;
-      const initialValue = -99;
-      mockServerGetCounterValueById(id, initialValue);
-
-      render(<MyAjaxCounter />);
-
-      screen.getByRole("spinbutton", { hidden: true });
-      screen.getByText(/Value/);
-
-      await waitForElementToBeRemoved(
-        () => screen.queryByRole("spinbutton", { hidden: true }),
-        { timeout: 2000 }
-      );
-
-      expect(screen.getByText(/Value/)).toHaveTextContent(`Value: ${initialValue}`);
+  fit('sould TODO', async () => {
+    const exectedCounterEntity: CounterValueEntity = {
+      id: 100,
+      value: 456
+    };
+    stubServerApi.stub({
+      method: 'get',
+      path: '/api/counter-values/100',
+      responseJson: exectedCounterEntity,
+      options: { delay: 200 }
     });
 
-    describe('increment click', () => {
-      it('should async increment value', async () => {
-        const id = 100;
-        const initialValue = 111;
-        mockServerGetCounterValueById(id, initialValue);
+    renderComponent();
 
-        render(<MyAjaxCounter />);
-        await waitForElementToBeRemoved(
-          () => screen.getByRole("spinbutton", { hidden: true })
-        );
+    const valElBefore = await screen.findByText(/Value/i);
+    expect(valElBefore).toHaveTextContent(/Value:$/i);
 
-        mockServerPatchCounterValueById(id, { value: initialValue + 1 });
+    await screen.findByRole('spinbutton', { hidden: true });
 
-        userEvent.click(screen.getByText('Increment'));
-        await screen.findByRole("spinbutton", { hidden: true });
-        await waitForElementToBeRemoved(
-          () => screen.getByRole("spinbutton", { hidden: true })
-        );
+    await waitForElementToBeRemoved(
+      () => screen.queryByRole('spinbutton', { hidden: true }),
+      { timeout: 2000 }
+    );
 
-        expect(screen.getByText(/Value/)).toHaveTextContent(`Value: ${initialValue + 1}`);
-      });
-    });
+    const valEl = await screen.findByText(/Value/i);
+    expect(valEl).toHaveTextContent(/456/i);
 
-    describe('decrement click', () => {
-      it('should async decrement value', async () => {
-        const id = 100;
-        const initialValue = 123;
-        mockServerGetCounterValueById(id, initialValue);
 
-        render(<MyAjaxCounter />);
-        await waitForElementToBeRemoved(
-          () => screen.getByRole("spinbutton", { hidden: true })
-        );
-
-        mockServerPatchCounterValueById(id, { value: initialValue - 1 });
-
-        userEvent.click(screen.getByText('Decrement'));
-        await screen.findByRole("spinbutton", { hidden: true });
-        await waitForElementToBeRemoved(
-          () => screen.getByRole("spinbutton", { hidden: true })
-        );
-
-        expect(screen.getByText(/Value/)).toHaveTextContent(`Value: ${initialValue - 1}`);
-      });
-    });
+    // expect(false).toEqual(true);
   });
 
 });
 
-function mockServerGetCounterValueById(id: number, initialValue: number): void {
-  stubServerApi.stub({
-    method: 'get',
-    path: `/api/counter-values/${id}`,
-    responseJson: { id, value: initialValue }
-  });
-}
-
-function mockServerPatchCounterValueById(id: number, params: CounterValueParams) {
-  stubServerApi.stub({
-    method: 'patch',
-    path: `/api/counter-values/${id}`,
-    responseJson: params
-  });
+function renderComponent() {
+  render(<MyAjaxCounter />)
 }
