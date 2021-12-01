e23a6561a00f42d32ca308453db6d9b9186ef713
pwoloszun <piotr.woloszun@gmail.com>
2021-12-01 13:40:52 +0100

    counter p2

--- src/features/03-component-state/components/MySimpleCounter/index.tsx
+++ src/features/03-component-state/components/MySimpleCounter/index.tsx
@@ -1,19 +1,62 @@
-/*eslint @typescript-eslint/no-unused-vars: 'off'*/
-
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 import styles from './styles.module.css';
+import { produce } from 'immer';
+
+
+// const arr = ['a', 'b', 'c'];
+
+// const el0 = arr[0];
+// const el1 = arr[1];
+// const el2 = arr[2];
+
+// const [el0, , el2] = arr;
+
+// pseudo code
+const _state = {
+  0: 210,
+  1: 'bob',
+  2: { name: '12345654667867867', id: 123 }
+};
+
+
+// 1: render phase
+// 2: commit phase
+// painted in the browser
+// single source of truth
 
 export default function MySimpleCounter(): React.ReactElement {
-  const value = 100;
+  console.log('RENDER:',);
+  const [value, setValue] = useState(200); // state piece
+  const [name, setName] = useState('bob'); // local piece of state
+  const [person, setPerson] = useState({
+    name: 'batman',
+    id: 123,
+    children: [{ id: 456, name: 'ed' }]
+  }); // local piece of state
+
   const incrementHandler = () => {
-    // TODO
+    console.log('BTN CLICK');
+    setValue((currValue) => {
+      const nextValue = currValue + 10;
+      return nextValue;
+    });
+
+    setPerson((currPerson) => {
+      const nextPerson = produce(currPerson, (draft) => {
+        draft.name = `bat ${Math.random()}`;
+        draft.children[0].name = 'ed 22';
+      });
+      return nextPerson;
+    });
   };
 
   return (
     <div className={styles.mySimpleCounter}>
-      TODO_PLACEHOLDER
-      <button>incr</button>
+      <h3>Value: {value}</h3>
+      <h3>name: {name}</h3>
+      <h3>person: {person.name}</h3>
+      <button onClick={incrementHandler}>incr</button>
     </div>
   );
 }
