c43c3c9f66a1f12440983023d504de074f46af14
pwoloszun <piotr.woloszun@gmail.com>
2021-12-01 12:22:13 +0100

    personal data

--- src/features/01-component-basics/components/PersonalData/index.tsx
+++ src/features/01-component-basics/components/PersonalData/index.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 
 import styles from './styles.module.css';
 
-// const user = {
-//   name: 'Batman',
-//   age: 43,
-//   email: 'bruce.w@heroes.com',
-//   url: 'https://i.pinimg.com/originals/1c/be/c4/1cbec4f304cf609d2346d3c5f2ff8b22.jpg'
-// };
+
 
 // TODO: PersonalData component
+
+export default function PersonalData(): React.ReactElement {
+  const user = {
+    name: 'Batman',
+    age: 43,
+    email: 'bruce.w@heroes.com',
+    url: 'https://i.pinimg.com/originals/1c/be/c4/1cbec4f304cf609d2346d3c5f2ff8b22.jpg'
+  };
+
+  return (
+    <div className={styles.persnalData}>
+      <p>Name: {user.name}</p>
+      <img src={user.url} alt={user.name} />
+    </div>
+  );
+}
+
