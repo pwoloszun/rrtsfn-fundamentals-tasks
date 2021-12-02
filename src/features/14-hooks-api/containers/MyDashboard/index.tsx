4559ddee305edbcc48eb87fe7fb962cb6942fb34
pwoloszun <piotr.woloszun@gmail.com>
2021-12-02 15:45:31 +0100

    useAsync impl

--- src/features/14-hooks-api/containers/MyDashboard/index.tsx
+++ src/features/14-hooks-api/containers/MyDashboard/index.tsx
@@ -1,34 +1,11 @@
-import React, { useState, useEffect } from 'react';
-
-import fetchRealEstates, { RealEstate } from 'src/api/fetch-real-estates';
-import fetchUsers, { User } from 'src/api/fetch-users';
+import React from 'react';
+import fetchRealEstates from 'src/api/fetch-real-estates';
+import fetchUsers from 'src/api/fetch-users';
 
 import EntitiesSimpleList from '../../components/EntitiesSimpleList';
 import useAsync from '../../hooks/useAsync';
 
 export default function MyDashboard() {
-  // const [realEstates, setRealEstates] = useState<RealEstate[]>([]);
-  // // TODO 1
-  // const isLoading = true;;
-  // const error = null;
-
-  // // TODO 1: impl fetching real estates
-  // useEffect(() => {
-  //   fetchRealEstates()
-  //     .then((realEstates) => {
-  //       console.log('re:', realEstates);
-  //     })
-  //     .catch((err) => {
-  //       // TODO
-  //     })
-  //     .finally(() => {
-  //       // TODO ?
-  //     });
-  // }, []);
-
-
-
-  // TODO 2: refactor
   const [
     realEstates,
     isRealEstatesLoading,
