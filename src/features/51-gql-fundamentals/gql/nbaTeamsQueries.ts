aeec6bc3e85f48c1e13c1669604f7268af91cec8
pwoloszun <piotr.woloszun@gmail.com>
2021-12-15 13:53:50 +0100

    quereies p1

--- src/features/51-gql-fundamentals/gql/nbaTeamsQueries.ts
+++ src/features/51-gql-fundamentals/gql/nbaTeamsQueries.ts
@@ -1,10 +1,24 @@
 import { gql } from '@apollo/client';
 
+export const GetAllTeams = gql`
+  query GetAllTeams {
+    nbaTeams {
+      abbreviation
+      full_name
+
+      conference
+      division
+  
+    }
+  }
+`;
+
 export const GetTeam = gql`
   query GetTeam($nbaTeamId: ID!) {
     nbaTeam(id: $nbaTeamId) {
       abbreviation
       full_name
+      city
 
       # TODO players
     }
--- src/features/51-gql-fundamentals/pages/BatchQueryExamplePage.tsx
+++ src/features/51-gql-fundamentals/pages/BatchQueryExamplePage.tsx
@@ -5,13 +5,14 @@ import { useQuery } from '@apollo/client';
 import Loadable from 'src/components/Loadable';
 import PrettyPrintJson from 'src/components/PrettyPrintJson';
 
-import { GetTeam, GetPlayers } from '../gql/nbaTeamsQueries';
+import { GetTeam, GetPlayers, GetAllTeams } from '../gql/nbaTeamsQueries';
 
 const PLAYER_IDS = ['237', '434', '420', '117', '400', '472', '15'];
 
 const TEAM_ID = '2';
 
 export default function BatchQueryExamplePage(): React.ReactElement {
+  // const allTeamsResp = useQuery(GetAllTeams);
   const teamResp = useQuery(GetTeam, {
     variables: { nbaTeamId: TEAM_ID }
   });
@@ -22,10 +23,16 @@ export default function BatchQueryExamplePage(): React.ReactElement {
     <div>
       <h3>BatchQueryExample</h3>
 
-      {/* <Loadable isLoading={teamResp.loading} error={teamResp.error}>
-        <h5>NBA Team</h5>
+      {/* <Loadable isLoading={allTeamsResp.loading} error={allTeamsResp.error}>
+        <h5>NBA ALL Teams</h5>
+        <PrettyPrintJson data={allTeamsResp.data} />
+      </Loadable>
+      <hr /> */}
+
+      <Loadable isLoading={teamResp.loading} error={teamResp.error}>
+        <h5>Team</h5>
         <PrettyPrintJson data={teamResp.data} />
-      </Loadable> */}
+      </Loadable>
       <hr />
 
     </div>
