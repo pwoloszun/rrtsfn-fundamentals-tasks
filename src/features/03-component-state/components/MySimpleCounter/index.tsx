7b8d0595372379c962b3baaf7d5d75bf0e34a956
pwoloszun <piotr.woloszun@gmail.com>
2022-01-31 12:28:18 +0100

    immutable example

--- src/features/03-component-state/components/MySimpleCounter/index.tsx
+++ src/features/03-component-state/components/MySimpleCounter/index.tsx
@@ -6,24 +6,34 @@ import styles from './styles.module.css';
 
 // pseudo code
 const _state = {
-  [0]: 110
+  [0]: 110,
+  [1]: { name: 'batman 122314312', age: 123 }
 };
 
 export default function MySimpleCounter(): React.ReactElement {
   const [value, setValue] = useState(100); // piece of state
+  const [person, setPerson] = useState({ name: 'bob', age: 123 }); // piece of state
 
   const incrementHandler = () => {
-    setValue((currValue) => {
-      const nextValue = currValue + 10;
-      return nextValue;
-    });
+    // setValue((currValue) => {
+    //   const nextValue = currValue + 10;
+    //   return nextValue;
+    // });
 
-    setValue((currValue) => currValue + 10);
+    setPerson((currPerson) => {
+      const nextPerson = {
+        ...currPerson,
+        name: `batmqan ${Math.random()}`
+      };
+      return nextPerson;
+    });
   };
 
   return (
     <div className={styles.mySimpleCounter}>
       <h3>Value: {value}</h3>
+      <h3>Name: {person.name}</h3>
+
       <button onClick={incrementHandler}>incr</button>
     </div>
   );
