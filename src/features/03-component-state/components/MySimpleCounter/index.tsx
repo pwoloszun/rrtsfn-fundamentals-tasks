0ff4757c8d3bf45a717866a918fa89d702d707e2
pwoloszun <piotr.woloszun@gmail.com>
2022-02-14 16:58:20 +0100

    immutable impl

--- src/features/03-component-state/components/MySimpleCounter/index.tsx
+++ src/features/03-component-state/components/MySimpleCounter/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 import styles from './styles.module.css';
+import { produce } from 'immer';
+import { cloneDeep } from 'lodash';
 
 
 // PSEUDO code
@@ -8,7 +10,7 @@ let internalIndex = 0;
 
 const _state = {
   [0]: 210, // piece of state
-  // [1]: { name: 'batman', age: 1234 }
+  [1]: { name: 'batman', age: 1234 }
 };
 
 export default function MySimpleCounter(): React.ReactElement {
@@ -16,23 +18,31 @@ export default function MySimpleCounter(): React.ReactElement {
   // local state
   // SINGLE source of truth
   const [value, setValue] = useState(200); // piece of state
-  // const [person, setPerson] = useState({ name: 'batman', age: 1234 }); // piece of state
+  const [person, setPerson] = useState({ name: 'batman', age: 1234 }); // piece of state
 
 
   const incrementHandler = () => {
     console.log('click:',);
 
-    setValue((currValue) => {
-      const nextValue = currValue + 10;
-      return nextValue;
-    });
+    // setValue((currValue) => {
+    //   const nextValue = currValue + 10;
+    //   return nextValue;
+    // });
 
-    setValue((currValue) => currValue + 10);
+    setPerson((currPerson) => {
+      const nextPerson = produce(currPerson, (draft) => {
+        draft.name = `batman ${Math.random()}`;
+      });
+
+      return nextPerson;
+    }); // ===
+
   };
 
   return (
     <div className={styles.mySimpleCounter}>
       <h3>Value: {value}</h3>
+      <h3>NAme: {person.name}</h3>
       <button onClick={incrementHandler}>incr</button>
     </div>
   );
