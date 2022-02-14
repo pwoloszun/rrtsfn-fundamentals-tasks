03c1229f8268b55e60ba0c5dbea8476f2b8ea4c5
pwoloszun <piotr.woloszun@gmail.com>
2022-02-14 17:28:03 +0100

    toggle naive impl

--- src/features/03-component-state/components/MyToggleText/index.tsx
+++ src/features/03-component-state/components/MyToggleText/index.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './index.module.css';
 
 export default function MyToggleText(): React.ReactElement {
-  // TODO: init component LOCAL state
-  // TODO css classes: 'btn-danger', 'btn-primary'
+  const [isVisible, setIsVisible] = useState(true);
+  const [btnCssClass, setBtnCssClass] = useState('btn-danger');
+  const [btnText, setBtnText] = useState('Toggle Hide');
+
+  const btnClickHandler = () => {
+    if (isVisible) {
+      setIsVisible(false);
+      setBtnCssClass('btn-primary');
+      setBtnText('Toggle Show');
+    } else {
+      setIsVisible(true);
+      setBtnCssClass('btn-danger');
+      setBtnText('Toggle Hide');
+    }
+  };
 
   return (
     <div className={styles.myToggleText}>
-      <button className="btn">
-        Toggle TODO
+      <button className={`btn ${btnCssClass}`} onClick={btnClickHandler}>
+        {btnText}
       </button>
       <hr />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis cursus nisi eu sapien finibus elementum. Integer
-        porta mi orci, congue malesuada velit varius eu. Fusce quis ante fermentum, tempus nisi nec, luctus neque.
-        Maecenas in laoreet ante, aliquet rutrum justo. Phasellus placerat felis erat, et dapibus libero mollis eget.
-        Nunc nec dictum lacus. Fusce congue nisi sit amet tristique vestibulum. Aenean congue diam eu lectus laoreet
-        molestie. Morbi leo odio, gravida quis hendrerit non, volutpat eget quam. Nulla sagittis lacus sit amet diam
-        lacinia sodales. In fermentum elementum interdum. Donec sit amet venenatis augue. Aliquam suscipit sagittis
-        mauris sed vestibulum. Donec facilisis vestibulum venenatis. Proin venenatis libero vel feugiat ultrices.
-      </p>
+      {
+        isVisible && <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis cursus nisi eu sapien finibus elementum. Integer
+          porta mi orci, congue malesuada velit varius eu. Fusce quis ante fermentum, tempus nisi nec, luctus neque.
+          Maecenas in laoreet ante, aliquet rutrum justo. Phasellus placerat felis erat, et dapibus libero mollis eget.
+          Nunc nec dictum lacus. Fusce congue nisi sit amet tristique vestibulum. Aenean congue diam eu lectus laoreet
+          molestie. Morbi leo odio, gravida quis hendrerit non, volutpat eget quam. Nulla sagittis lacus sit amet diam
+          lacinia sodales. In fermentum elementum interdum. Donec sit amet venenatis augue. Aliquam suscipit sagittis
+          mauris sed vestibulum. Donec facilisis vestibulum venenatis. Proin venenatis libero vel feugiat ultrices.
+        </p>
+      }
     </div>
   );
 }
