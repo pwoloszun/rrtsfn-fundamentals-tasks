2f8b6fde7dae60671de9eb9bf73f17931d0d400f
pwoloszun <piotr.woloszun@gmail.com>
2022-02-16 15:32:00 +0100

    my dashboard task

--- src/features/14-hooks-api/containers/MyDashboard/index.tsx
+++ src/features/14-hooks-api/containers/MyDashboard/index.tsx
@@ -1,49 +1,53 @@
 import React, { useState, useEffect } from 'react';
 
 import fetchRealEstates, { RealEstate } from 'src/api/fetch-real-estates';
+import fetchUsers, { User } from 'src/api/fetch-users';
 
 import EntitiesSimpleList from '../../components/EntitiesSimpleList';
+import useAsync from '../../hooks/useAsync';
 
 export default function MyDashboard() {
-  // TODO 1
-  const realEstates: RealEstate[] = [];
-  const isLoading = true;;
-  const error = null;
-
-  // TODO 1: impl fetching real estates
-
-
   // TODO 2: refactor
-  // const [
-  //   realEstates,
-  //   isRealEstatesLoading,
-  //   realEstatesError
-  // ] = useAsync(fetchRealEstates);
+  // fetchRealEstates()
+  //   .then((realEstatesData) => {
+  //     console.log('re data:', realEstatesData);
+  //   })
+  //   .catch((err) => {
+  //     console.log('some err:', err);
+  //   })
+  //   .finally(() => {
+  //     console.log('finally call');
+  //   });
 
-  // const [
-  //   users,
-  //   isUsersLoading,
-  //   usersError
-  // ] = useAsync(fetchUsers);
 
+  const [
+    realEstates,
+    isRealEstatesLoading,
+    realEstatesError
+  ] = useAsync(fetchRealEstates);
 
+  const [
+    users,
+    isUsersLoading,
+    usersError
+  ] = useAsync(fetchUsers);
 
   return (
     <div>
       <h3>MyDashboard</h3>
       <EntitiesSimpleList
         entites={realEstates}
-        isLoading={isLoading}
-        error={error}
+        isLoading={isRealEstatesLoading}
+        error={realEstatesError}
         renderValue="street"
       />
       <hr />
-      {/* <EntitiesSimpleList
+      <EntitiesSimpleList
         entites={users}
         isLoading={isUsersLoading}
         error={usersError}
         renderValue="email"
-      /> */}
+      />
     </div>
   );
 }
