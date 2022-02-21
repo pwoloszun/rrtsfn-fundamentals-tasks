46ef0393d1180f07674e98faf4b17f72c9723fd3
pwoloszun <piotr.woloszun@gmail.com>
2022-02-21 17:25:37 +0100

    mycounter example part 1

--- src/features/13-testing-tasks/examples/__tests__/MyCounter.spec.tsx
+++ src/features/13-testing-tasks/examples/__tests__/MyCounter.spec.tsx
@@ -1,15 +1,31 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { merge } from 'lodash';
 
 import MyCounter, { IMyCounterProps } from '../MyCounter';
 
 describe('MyCounter', () => {
 
-  xit('should render passed value', () => {
-    expect(false).toEqual(true);
+  beforeEach(() => {
+
   });
 
-  xit('should emit onIncrement event on "Increment" btn click', () => {
+  it('should render passed value', async () => {
+    const props = generateProps({
+      value: 123
+    });
+    renderComponent(props);
+
+    const headerEl = await screen.findByText(/Value:/i);
+
+    expect(headerEl).toHaveTextContent(/Value: 123/i);
+    // expect(false).toEqual(true);
+  });
+
+  fit('should emit onIncrement event on "Increment" btn click', () => {
+    const props = generateProps();
+    renderComponent(props);
+
     expect(false).toEqual(true);
   });
 
@@ -19,6 +35,19 @@ describe('MyCounter', () => {
 
 });
 
-function renderMyCounter(props: IMyCounterProps) {
+// test helper
+function renderComponent(props: IMyCounterProps) {
   return render(<MyCounter {...props} />);
+}
+
+function generateProps(props: Partial<IMyCounterProps> = {}): IMyCounterProps {
+  const value = 456;
+  const onIncrement = () => { };
+  const onDecrement = () => { };
+
+  const defaultProps: IMyCounterProps = {
+    value, onDecrement, onIncrement
+  };
+
+  return merge({}, defaultProps, props);
 }
