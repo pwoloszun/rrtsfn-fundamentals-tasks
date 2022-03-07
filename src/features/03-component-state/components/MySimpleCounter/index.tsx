25db385170ba8dd57cf6586c66b8bb00c39a8ba8
pwoloszun <piotr.woloszun@gmail.com>
2022-03-07 12:13:59 +0100

    counter example p1

--- src/features/03-component-state/components/MySimpleCounter/index.tsx
+++ src/features/03-component-state/components/MySimpleCounter/index.tsx
@@ -1,19 +1,31 @@
-/*eslint @typescript-eslint/no-unused-vars: 'off'*/
-
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './styles.module.css';
 
+// pseudo code
+const _state = {
+  [0]: 110,
+  [1]: { name: 'bob', age: 123 },
+  // [2]: 'qq',
+};
+
 export default function MySimpleCounter(): React.ReactElement {
-  const value = 100;
+  const [value, setValue] = useState(100); // state piece
+  const [person, setPerson] = useState({ name: 'bob', age: 123 }); // state piece
+  // const [name, setName] = useState('qq'); // state piece
+
   const incrementHandler = () => {
-    // TODO
+    console.log('click:',);
+    // 1. INSTANT! modify internal state
+    // 2. mark to re-render
+    setValue(value + 10);
+    console.log('AFTER:',);
   };
 
-  return (
+  return ( // snapshot 
     <div className={styles.mySimpleCounter}>
-      TODO_PLACEHOLDER
-      <button>incr</button>
+      <h3>Value: {value}</h3>
+      <button onClick={incrementHandler}>incr</button>
     </div>
   );
 }
