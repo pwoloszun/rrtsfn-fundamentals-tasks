8fdb9c1b57b8451f697ae5cc22032b5b64293fb3
pwoloszun <piotr.woloszun@gmail.com>
2022-03-07 12:42:49 +0100

    deep copy example

--- src/features/03-component-state/components/MySimpleCounter/index.tsx
+++ src/features/03-component-state/components/MySimpleCounter/index.tsx
@@ -1,10 +1,12 @@
+import { clone, cloneDeep } from 'lodash';
 import React, { useState } from 'react';
 
 import styles from './styles.module.css';
+import produce from 'immer';
 
 // pseudo code
 const _state = {
-  [0]: 110,
+  [0]: 100,
   [1]: { name: 'bob', age: 123 },
   // [2]: 'qq',
 };
@@ -18,13 +20,22 @@ export default function MySimpleCounter(): React.ReactElement {
     console.log('click:',);
     // 1. INSTANT! modify internal state
     // 2. mark to re-render
-    setValue(value + 10);
-    console.log('AFTER:',);
+    // setValue(value + 10);
+
+    // WRONG!!!! NEVER mutate comp. state !!!!
+    // person.name = `batman ${Math.random()}`;
+    const nextPerson = produce(person, (draft) => {
+      draft.name = `batman ${Math.random()}`;
+      draft.age = 456;
+    });
+    setPerson(nextPerson); // ===
+    console.log('AFTER:', person);
   };
 
   return ( // snapshot 
     <div className={styles.mySimpleCounter}>
       <h3>Value: {value}</h3>
+      <h3>Name: {person.name}</h3>
       <button onClick={incrementHandler}>incr</button>
     </div>
   );
