825e714b2916b5c48e4928db82fe2efad7eeab76
pwoloszun <piotr.woloszun@gmail.com>
2022-03-07 12:58:58 +0100

    cmp state example

--- src/features/03-component-state/components/MySimpleCounter/index.tsx
+++ src/features/03-component-state/components/MySimpleCounter/index.tsx
@@ -6,30 +6,37 @@ import produce from 'immer';
 
 // pseudo code
 const _state = {
-  [0]: 100,
-  [1]: { name: 'bob', age: 123 },
-  // [2]: 'qq',
+  [0]: 110,
+  // [1]: { name: 'bob', age: 123 },
 };
 
 export default function MySimpleCounter(): React.ReactElement {
+  console.log('RENDER:',);
+  // state driven
+  // state === SINLGE SOURCE OF TRUTH
   const [value, setValue] = useState(100); // state piece
   const [person, setPerson] = useState({ name: 'bob', age: 123 }); // state piece
-  // const [name, setName] = useState('qq'); // state piece
 
   const incrementHandler = () => {
     console.log('click:',);
     // 1. INSTANT! modify internal state
     // 2. mark to re-render
-    // setValue(value + 10);
+
+    setValue((currValue) => {
+      const nextValue = currValue + 10;
+      return nextValue;
+    });
+    setValue((currValue) => currValue + 10);
 
     // WRONG!!!! NEVER mutate comp. state !!!!
     // person.name = `batman ${Math.random()}`;
+
+    // GOOD
     const nextPerson = produce(person, (draft) => {
       draft.name = `batman ${Math.random()}`;
       draft.age = 456;
     });
     setPerson(nextPerson); // ===
-    console.log('AFTER:', person);
   };
 
   return ( // snapshot 
