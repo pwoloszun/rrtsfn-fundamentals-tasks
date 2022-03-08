de2720689493764e8449edc27b8aa8e1340e3d1d
pwoloszun <piotr.woloszun@gmail.com>
2022-03-08 11:56:58 +0100

    useHist impl

--- src/features/14-hooks-api/hooks/useHistory.ts
+++ src/features/14-hooks-api/hooks/useHistory.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { last, first } from 'lodash';
+import produce, { Draft } from 'immer';
 
 interface UseHistoryResult<T> {
   value: T;
@@ -13,23 +14,61 @@ interface UseHistoryResult<T> {
 };
 
 export default function useHistory<T>(initialValue: T): UseHistoryResult<T> {
-  // TODO
+  const [currentValue, setCurrentValue] = useState(initialValue);
+  const [future, setFuture] = useState<T[]>([]);
+  const [past, setPast] = useState<T[]>([]);
+
+  const canUndo = past.length > 0;
+  const canRedo = future.length > 0;
+
+
+  const setValue = (value: T) => {
+    const nextPast = [...past, currentValue];
+    setPast(nextPast);
+
+    const nextValue = value;
+    setCurrentValue(nextValue);
+
+    const nextFuture: T[] = [];
+    setFuture(nextFuture);
+  };
+
+  const undo = () => {
+    if (!canUndo) {
+      return;
+    }
+    const nextPast = past.slice(0, -1);
+    setPast(nextPast);
+
+    const nextValue = past[past.length - 1];
+    setCurrentValue(nextValue);
+
+    const nextFuture = [currentValue, ...future];
+    setFuture(nextFuture);
+  };
+
+  const redo = () => {
+    if (!canRedo) {
+      return;
+    }
+    const nextPast = [...past, currentValue];
+    setPast(nextPast);
+
+    const nextValue = future[0];
+    setCurrentValue(nextValue);
+
+    const nextFuture = future.slice(1);
+    setFuture(nextFuture);
+  };
 
   return {
-    // part 1
-    value: initialValue,
-    setValue: (nextValue: T) => { },
-    past: [],
-
-    // part 2
-    undo: () => { },
-
-    // part 3
-    future: [],
-    redo: () => { },
-
-    // part 4
-    canUndo: false,
-    canRedo: false,
+    value: currentValue,
+    setValue,
+    past,
+    undo,
+    future,
+    redo,
+    canUndo,
+    canRedo,
   };
 }
