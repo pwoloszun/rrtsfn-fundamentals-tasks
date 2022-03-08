e00cf084fa0f81cee42db9d02da780a60829e2ac
pwoloszun <piotr.woloszun@gmail.com>
2022-03-08 14:57:48 +0100

    useAsync task

--- src/features/14-hooks-api/index.tsx
+++ src/features/14-hooks-api/index.tsx
@@ -29,7 +29,7 @@ export default function HooksApi() {
       </div>
       <div>
         {/* <LoadManyThings /> */}
-        {/* <MyDashboard /> */}
+        <MyDashboard />
       </div>
       <div>
         {/* <SearchWithDebounce /> */}
