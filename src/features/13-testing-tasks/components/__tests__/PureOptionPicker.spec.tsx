df7a02d92f13810d4d2462983c1e5b4e213dacde
pwoloszun <piotr.woloszun@gmail.com>
2022-03-09 10:11:37 +0100

    render option picker inputs spec impl

--- src/features/13-testing-tasks/components/__tests__/PureOptionPicker.spec.tsx
+++ src/features/13-testing-tasks/components/__tests__/PureOptionPicker.spec.tsx
@@ -6,15 +6,24 @@ import PureOptionPicker, { IPureOptionPickerProps } from '../PureOptionPicker';
 
 describe('PureOptionPickerComponent', () => {
 
-  fit('should render input title & button for each input item', async () => {
-    // TODO: render PureOptionPicker
+  it('should render input title & button for each input item', async () => {
+    const props = generateProps({
+      title: 'My Options Title'
+    });
+    renderComponent(props);
 
-    // test renderd title
+    await screen.findByText(/My Options Title/i);
 
-    // test renderd items
+    const optionBtns = await screen.findAllByRole('button', { hidden: true });
 
+    const { items } = props;
 
-    expect(false).toEqual(true);
+    expect(optionBtns.length).toEqual(items.length);
+
+    items.forEach((item, index) => {
+      const btn = optionBtns[index];
+      expect(btn).toHaveTextContent(item.text);
+    });
   });
 
   xit('should render button for each input item', async () => {
@@ -37,12 +46,28 @@ describe('PureOptionPickerComponent', () => {
 
 type Props = IPureOptionPickerProps<any>;
 
-// TODO
+function renderComponent(props: Props) {
+  render(<PureOptionPicker {...props} />);
+}
 
-// const items = [
-//   { id: 100, text: 'first item' },
-//   { id: 200, text: 'second item' },
-//   { id: 300, text: 'third item' },
-//   { id: 400, text: 'fourth item' },
-//   { id: 500, text: 'fifth item' },
-// ];
+function generateProps(props: Partial<Props> = {}): Props {
+  const title = 'default test title';
+  const items = [
+    { id: 100, text: 'first item' },
+    { id: 200, text: 'second item' },
+    { id: 300, text: 'third item' },
+    { id: 400, text: 'fourth item' },
+    { id: 500, text: 'fifth item' },
+  ];
+  const selectedItem = null;
+  const onItemSelect = jest.fn();
+  const defaulProps: Props = {
+    title,
+    items,
+    selectedItem,
+    onItemSelect,
+  };
+  return merge({}, defaulProps, props);
+}
+
+
