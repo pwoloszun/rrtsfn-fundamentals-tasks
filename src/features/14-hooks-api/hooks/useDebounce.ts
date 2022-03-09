62f4aa134db3c34c7db1ef8cf0e4c218a1a9939a
pwoloszun <piotr.woloszun@gmail.com>
2022-03-09 11:08:40 +0100

    smart quick search fix

--- src/features/14-hooks-api/hooks/useDebounce.ts
+++ src/features/14-hooks-api/hooks/useDebounce.ts
@@ -3,7 +3,18 @@ import { useState, useEffect } from 'react';
 type UseDebounceResult<T> = [T, (nextValue: T) => void];
 
 export default function useDebounce<T>(initialValue: T, delayInMs: number): UseDebounceResult<T> {
-  // TODO
+  const [value, setValue] = useState(initialValue);
+  const [debouncedValue, setDebouncedValue] = useState(initialValue);
 
-  return [initialValue, (nextValue: T) => { }];
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delayInMs);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value, delayInMs]);
+
+  return [debouncedValue, setValue];
 }
